dc4c4554a0820d60077ebc016bcc3a943fd655c7
Barend Emmerzaal <b.emmerzaal@gmail.com>
2026-03-21 08:30:35 +0100

    Initial Commit

---
+++ WhisperTestTests/WhisperTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperTestTests.swift
+//  WhisperTestTests
+//
+//  Created by B.P. Emmerzaal on 21/03/2026.
+//
+
+import Testing
+@testable import WhisperTest
+
+struct WhisperTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
